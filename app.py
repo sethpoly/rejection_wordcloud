a68daa261608c268e724745bb8bc9737f38473ed
sethpoly <emailspolyniak@gmail.com>
2021-05-18 15:51:53 -0400

    Add .idea to gitignore

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 import matplotlib.pyplot as plt
+
+import service_account as acc
+
+data = acc.Spreadsheet('Applications', 'Rejections').sheet
